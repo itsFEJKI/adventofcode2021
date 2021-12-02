c89df5517b3c557a997b57d0ddedb03c1fea6797
Ricky <itsfejki@gmail.com>
2021-12-02 21:12:09 +0100

    Sheeesh
    init

---
+++ day1/day1/day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Hello CarlG!");
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
