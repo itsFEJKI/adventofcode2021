761aaa0e9aa590df85c16dea2c7bdf2a054379b2
Ricky <itsfejki@gmail.com>
2021-12-03 10:08:00 +0100

    what the fuck is this
    what is this fucking shit how do I upload sexier?

---
+++ 2021/2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using _2021.tasks;
+
+namespace _2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 - AoC 2021:");
+            Console.WriteLine("-----------------");
+            _ = new Day1();
+        }
+    }
+}
---
+++ 2021/2021/tasks/Day1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace _2021.tasks
+{
+    public class Day1
+    {
+        public Day1()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            string[] textArray = System.IO.File.ReadAllText(@"C:\Users\ricky\Desktop\adventofcode2021\2021\2021\tasks_inputs\day1.txt").Split("\n");
+            int[] dives = Array.ConvertAll(textArray, s => int.Parse(s));
+            stopwatch.Start();
+            Console.WriteLine("Part1: {0}", Part1(dives));
+            Console.WriteLine("Part2: {0}", Part2(dives));
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time: {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        int Part1(int[] dives)
+        {
+            int increments = 0;
+            for(int i = dives.GetLowerBound(0); i < dives.GetUpperBound(0); i++)
+            {
+                if (dives[i] < dives[i + 1])
+                    increments++;
+
+            }
+            return increments;
+        }
+
+        int Part2(int[] dives)
+        {
+            int prev = dives[0] + dives[1] + dives[2];
+            int increments = 0;
+
+            for (int i = 0; i < dives.Length - 2; i++)
+            {
+
+                if (prev < dives[i] + dives[i + 1] + dives[i + 2])
+                {
+                    increments++;
+                }
+
+                prev = dives[i] + dives[i + 1] + dives[i + 2];
+
+            }
+
+            return increments;
+        }
+    }
+}
